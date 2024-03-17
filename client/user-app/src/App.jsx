acc2a6c96cd877ea5864d31e9c6896c2ab24b261
longquan98@gmail.com <longquan98@gmail.com>
2024-03-17 16:10:10 -0400

    Build the User App

--- client/user-app/src/App.jsx
+++ client/user-app/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// user-app/src/App.jsx
+import "./App.css";
+import UserComponent from "./UserComponent";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4001/graphql", // Set this to your actual GraphQL endpoint
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <ApolloProvider client={client}>
+        <UserComponent />
+      </ApolloProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
